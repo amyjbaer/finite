ed1feaa64e81bce890affab0ff5dd05017552ed7
Amy Baer <amyjbaer@gmail.com>
2023-11-05 13:22:06 -0800

    fix update server

--- server/app.py
+++ server/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
-import time
 from flask import request
 import json
 import requests
+import git
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../client/build', static_url_path='/')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 @app.route("/api/auth")
 def auth():
@@ -14,7 +18,7 @@ def auth():
             'client_id': (None, '283690477978497'),
             'client_secret': (None, '1b3911f47e062fb0ab2427428de713ef'),
             'grant_type': (None, 'authorization_code'),
-            'redirect_uri': (None, 'https://willowy-biscuit-0a7ec9.netlify.app/api/auth'),
+            'redirect_uri': (None, 'https://amyjbaer.pythonanywhere.com/api/auth'),
             'code': (None, code),
         }
 
@@ -22,6 +26,16 @@ def auth():
         return response.text
     except Exception as e:
         return json.dumps(e)
+
+@app.route('/api/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('..')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 if __name__ == "__main__":
     app.run(debug=True)
